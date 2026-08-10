fbd0da7f3e3e99074846f45b2c1491ef34b49656
Gerteg <gertsmik@gmail.com>
2026-08-11 00:43:01 +0500

    First change

---
+++ MN_Fourier.c
@@ -0,0 +1,8 @@
+#include "MN_Fourier.h"
+
+void F1(){
+    sprintf (buf1, "F1!");
+};
+void F2(){ 
+    sprintf (buf2, "F2!");
+};
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>   // для sprintf и puts
+
+int main() {
+
+    return 0;
+}
